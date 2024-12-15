670e6edc8c2526adef1fad04a7bb3c0a4953db3c
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-12-15 11:07:30 +0500

    landing page

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
 AOS.init()
 
+// popup window --------------------------------------------------------------------------------------
+const openPopupButton = document.getElementById("open-popup")
+const closePopupButton = document.getElementById("close-popup")
+const popup = document.getElementById("popup")
+
+openPopupButton.addEventListener("click", () => {
+    popup.classList.add("show")
+})
+
+closePopupButton.addEventListener("click", () => {
+    popup.classList.remove("show")
+})
+
+popup.addEventListener("click", (event) => {
+    if (event.target === popup) {
+        popup.classList.remove("show")
+    }
+})
+
+// tel number regex --------------------------------------------------------------------------------
+document.querySelector(".input-field").addEventListener("input", (event) => {
+    const input = event.target
+    let value = input.value.replace(/\D/g, "")
+
+    if (value.length > 2) {
+        value = `(${value.substring(0, 2)}) ${value.substring(2)}`
+    }
+    if (value.length > 8) {
+        value = `${value.substring(0, 8)}-${value.substring(8)}`
+    }
+    if (value.length > 11) {
+        value = `${value.substring(0, 11)}-${value.substring(11, 13)}`
+    }
+
+    input.value = value
+})
+
 // video pause and play btn ------------------------------------------------------------------------
 const videos = document.querySelectorAll(".video")
 const playButtons = document.querySelectorAll(".playBtn")
